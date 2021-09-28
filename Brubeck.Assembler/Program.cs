2a137be773956d5c8eecd08ad547edc5735394e3
Johann Scott <75587365+somecollagist@users.noreply.github.com>
2021-09-28 20:22:04 +0100

    Started work on the assembler

---
+++ Brubeck.Assembler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Brubeck.Assembler
+{
+    class Program
+    {
+        public static void Assemble(string path)
+        {
+            using StreamReader sr = new(path);
+            string[] code = sr
+                .ReadToEnd()
+                .Split('\n')
+                .Select(t => t.ToUpper())
+                .ToArray();
+            foreach(string cmd in code)
+            {
+                Console.WriteLine(cmd);
+            }
+        }
+    }
+}
