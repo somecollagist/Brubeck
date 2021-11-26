b94ed4ca89246bd994e980aa4450433752af0241
Ben M Jackson <Jackson6966@bradfordgrammar.com>
2021-11-26 13:04:59 +0000

    Refactoring Uniform grid to bitmap

--- Brubeck.Peripheral/Monitor.xaml.cs
+++ Brubeck.Peripheral/Monitor.xaml.cs
@@ -17,132 +17,123 @@ using Brubeck.Core;
 
 namespace Brubeck.Peripheral
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class Monitor : Window
-    {
-        /// <summary>
-        /// Width of the monitor in pixels.
-        /// </summary>
-        public static readonly int ResWidth = 240;
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class Monitor : Window
+	{
+		/// <summary>
+		/// Width of the monitor in pixels.
+		/// </summary>
+		public static readonly int ResWidth = 240;
 
-        /// <summary>
-        /// Height of the monitor in pixels.
-        /// </summary>
-        public static readonly int ResHeight = 184;
+		/// <summary>
+		/// Height of the monitor in pixels.
+		/// </summary>
+		public static readonly int ResHeight = 184;
 
-        /// <summary>
-        /// Scale factor of the monitor.
-        /// </summary>
-        /// <remarks>How many pixels on a real monitor make up a pixel on the emulated monitor.</remarks>
-        private const int ScaleFactor = 4;
+		/// <summary>
+		/// Scale factor of the monitor.
+		/// </summary>
+		/// <remarks>How many pixels on a real monitor make up a pixel on the emulated monitor.</remarks>
+		private const int ScaleFactor = 4;
 
-        /// <summary>
-        /// Constructor for a monitor.
-        /// </summary>
-        public Monitor()
-        {
-            InitializeComponent();
+		/// <summary>
+		/// Constructor for a monitor.
+		/// </summary>
+		public Monitor()
+		{
+			InitializeComponent();
 
-            //Set the grid's size to the size in "real pixels" i.e. number of pixels on a real monitor
-            Screen.Height = ResHeight * ScaleFactor;
-            Screen.Width = ResWidth * ScaleFactor;
+			//Set the grid's size to the size in "real pixels" i.e. number of pixels on a real monitor
+			Screen.Height = ResHeight * ScaleFactor;
+			Screen.Width = ResWidth * ScaleFactor;
 
-            //Dynamically create cells in the uniformgrid
-            Pixels.Rows = ResHeight;
-            Pixels.Columns = ResWidth;
+			//Dynamically create cells in the uniformgrid
+			Pixels.Rows = ResHeight;
+			Pixels.Columns = ResWidth;
 
-            //Make each cell of the uniformgrid a rectangle which can be coloured (i.e. a pixel)
-            for(int y = 0; y < ResHeight; y++)
-            {
-                for(int x = 0; x < ResWidth; x++)
-                {
-                    Pixels.Children.Insert((y * ResWidth) + x, new Rectangle());
-                }
-            }
-        }
+			//Make each cell of the uniformgrid a rectangle which can be coloured (i.e. a pixel)
+			for(int y = 0; y < ResHeight; y++)
+			{
+				for(int x = 0; x < ResWidth; x++)
+				{
+					Pixels.Children.Insert((y * ResWidth) + x, new Rectangle());
+				}
+			}
+		}
 
-        /// <summary>
-        /// Displays the given video signal.
-        /// </summary>
-        /// <param name="input">Qyte array of the visual signal to display.</param>
-        public void Display(Qyte[] input)
-        {
-            //Dispatcher.Invoke(() =>
-            //{
-            //    //Qyte[] might become Qit[] in future, since that'd align more closely with how it works in the real world
+		/// <summary>
+		/// Displays the given video signal.
+		/// </summary>
+		/// <param name="input">Qyte array of the visual signal to display.</param>
+		public void Display(Qyte[] input)
+		{
+			//Console.WriteLine("Before");
+			//Qit[] VisualQitMap = string.Join("", input.Select(t => t.ToString())).Select(t => QitConverter.GetQitFromChar(t)).ToArray();
+			//Console.WriteLine("After");
+			//for (int y = 0; y < ResHeight; y++)
+			//{
+			//	Dispatcher.Invoke(() =>
+			//	{
+			//		for(int x = 0; x < ResWidth; x++)
+			//		{
+			//			Dispatcher.Invoke(() =>
+			//			{
+			//				Pixels.Children.SetPixel(x, y, ResWidth, QitColourMapping[VisualQitMap[y * ResWidth + x]]);
+			//			});
+			//		}
+			//	});
+			//}
+			//Console.WriteLine("VRAM Written");
 
-            //    //Convert to qitmap since that's how we'll store the state
-            //    Qit[] VisualQitMap = string.Join("", input.Select(t => t.ToString())).Select(t => QitConverter.GetQitFromChar(t)).ToArray();
-            //    //Each qit will be used to set the colour of each pixel
-            //    for (int index = 0; index < VisualQitMap.Length - 3; index++)
-            //    {
-            //        Dispatcher.Invoke(() =>
-            //        {
-            //            Pixels.Children.SetPixel(index, QitColourMapping[VisualQitMap[index]]);
-            //        });
-            //    }
-            //});
+			Qit[] VisualQitMap = string.Join("", input.Select(t => t.ToString())).Select(t => QitConverter.GetQitFromChar(t)).ToArray();
 
-            Qit[] VisualQitMap = string.Join("", input.Select(t => t.ToString())).Select(t => QitConverter.GetQitFromChar(t)).ToArray();
+			WriteableBitmap bmp = new(ResWidth, ResHeight, 96 / ScaleFactor, 96 / ScaleFactor, PixelFormats.Bgr24, null);
+			bmp.WritePixels(VisualQitMap.Select(t => ))
+		}
 
-            for (int y = 0; y < ResHeight; y++)
-            {
-                Dispatcher.Invoke(() =>
-                {
-                    for(int x = 0; x < ResWidth; x++)
-                    {
-                        Dispatcher.Invoke(() =>
-                        {
-                            Pixels.Children.SetPixel(x, y, ResWidth, QitColourMapping[VisualQitMap[y * ResWidth + x]]);
-                        });
-                    }
-                });
-            }
-            Console.WriteLine("VRAM Written");
-        }
+		/// <summary>
+		/// Dictionary pairing Qits to colours.
+		/// </summary>
+		private static readonly Dictionary<Qit, byte[]> QitColourMapping = new()
+		{
+			//BGR
+			{ Qit.A, new byte[] { 0, 0, 0 } },
+			{ Qit.E, new byte[] { 0, 0, 0 } },
+			{ Qit.I, new byte[] { 0, 0, 0 } },
+			{ Qit.O, new byte[] { 0, 0, 0 } },
+			{ Qit.U, new byte[] { 0, 255, 0 } },
+		};
+	}
 
-        /// <summary>
-        /// Dictionary pairing Qits to colours.
-        /// </summary>
-        private static readonly Dictionary<Qit, SolidColorBrush> QitColourMapping = new()
-        {
-            {Qit.A, new(Color.FromRgb(0, 0, 0)) },  //Black
-            {Qit.E, new(Color.FromRgb(0, 0, 0)) },  //Black
-            {Qit.I, new(Color.FromRgb(0, 0, 0)) },  //Black
-            {Qit.O, new(Color.FromRgb(0, 0, 0)) },  //Black
-            {Qit.U, new(Color.FromRgb(0, 255, 0)) } //Green
-        };
-    }
+	internal static class PixelGridExtensionMethods
+	{
+		/// <summary>
+		/// Sets a pixel at a given coordinate to a certain colour.
+		/// </summary>
+		/// <param name="x">Pixel's altitude from the top.</param>
+		/// <param name="y">Pixel's offset from the left.</param>
+		/// <param name="colour">The colour to be assigned.</param>
+		public static void SetPixel(this UIElementCollection pixels, int x, int y, int ResWidth, SolidColorBrush colour)
+		{
+			pixels.SetPixel(y * ResWidth + x, colour);
+		}
 
-    internal static class PixelGridExtensionMethods
-    {
-        /// <summary>
-        /// Sets a pixel at a given coordinate to a certain colour.
-        /// </summary>
-        /// <param name="x">Pixel's altitude from the top.</param>
-        /// <param name="y">Pixel's offset from the left.</param>
-        /// <param name="colour">The colour to be assigned.</param>
-        public static void SetPixel(this UIElementCollection pixels, int x, int y, int ResWidth, SolidColorBrush colour)
-        {
-            pixels.SetPixel(y * ResWidth + x, colour);
-        }
-
-        /// <summary>
-        /// Sets a pixel at a given index to a certain colour. 
-        /// </summary>
-        /// <param name="index">The pixels index, computed left to right, top to bottom.</param>
-        /// <param name="colour">the colour to be assigned.</param>
-        /// <returns></returns>
-        public static void SetPixel(this UIElementCollection pixels, int index, SolidColorBrush colour)
-        {
-            //Task.Run(() => ((Rectangle)pixels[index]).Fill = colour);
-            //lock(pixels)
-            //{
-            //    ((Rectangle)pixels[index]).Fill = colour;
-            //}
-            ((Rectangle)pixels[index]).Fill = colour;
-        }
-    }
+		/// <summary>
+		/// Sets a pixel at a given index to a certain colour. 
+		/// </summary>
+		/// <param name="index">The pixels index, computed left to right, top to bottom.</param>
+		/// <param name="colour">the colour to be assigned.</param>
+		/// <returns></returns>
+		public static void SetPixel(this UIElementCollection pixels, int index, SolidColorBrush colour)
+		{
+			//Task.Run(() => ((Rectangle)pixels[index]).Fill = colour);
+			//lock(pixels)
+			//{
+			//    ((Rectangle)pixels[index]).Fill = colour;
+			//}
+			((Rectangle)pixels[index]).Fill = colour;
+		}
+	}
 }
